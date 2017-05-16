10f27b0cad3d11bb9b3f422c391e9d84673157ec
Simon Conway <simon.conway@capgemini.com>
2017-05-16 16:37:17 +0100

    Added a blank title and map component

---
+++ app/components/FqaApp.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var Title = require('Title');
+var Map = require('Map');
+
+
+var FqaApp = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Title/>
+        <Map/>
+      </div>
+    )
+  }
+});
+
+  module.exports = FqaApp;
---
+++ app/components/Title.jsx
@@ -0,0 +1,11 @@
+var React = require('react')
+
+var Title = () => {
+    return (
+      <div>
+        <h1>Title</h1>
+      </div>
+    )
+  };
+
+module.exports = Title;
